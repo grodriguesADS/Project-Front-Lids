b1af340fbce009ffbabe922fbe69902dd60a87ca
Guilherme Rodrigues <guilherme.ads.2022@gmail.com>
2023-06-28 13:15:39 -0300

    add MenuComponent

--- lids/src/App.js
+++ lids/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import CreateColaborate from './screens/colaborate/CreateColaborate';
 
 import Login from './screens/login/Login';
 
 function App() {
   return (
-    <Login />
+    <CreateColaborate/>
   );
 }
 
--- lids/src/screens/login/Login.js
+++ lids/src/screens/login/Login.js
@@ -10,23 +10,23 @@ import "primereact/resources/primereact.min.css";
 class Login extends React.Component {
     render() {
         return (
-            <div className="login-container">
-               <div className="login-logo" >
-                <div className="background-logo">
-                    <center>
-                    <SVGLogin />
-                    </center>
-                </div>
-                <div className="login-email">
-                <label htmlFor="email">E-mail</label>
-                    <InputText style={{marginTop: '10px',marginBottom: '30px',border: '1px solid #ff9900', width: '300px'}} type="text" className="p-inputtext-sm" />
-                   
-                <label htmlFor="password">Senha</label>
-                    <InputText style={{marginTop: '10px',border: '1px solid #ff9900', width: '300px'}} type="text" className="p-inputtext-sm"  />
-                    <Button style={{marginTop: '15px', marginLeft:'215px', background: '#ff9900', borderColor: '#ff9900', color: 'black'}} label="Login" />
-                </div>
-               </div>
-            </div>
+    <div className="login-container">
+      <div className="login-logo">
+        <div className="background-logo">
+          <center>
+            <SVGLogin className="responsive-svg"/>
+          </center>
+        </div>
+        <div className="login-email">
+          <label htmlFor="email">E-mail</label>
+          <InputText style={{ marginTop: '10px', marginBottom: '30px', border: '1px solid #ff9900', width: '300px' }} type="text" className="p-inputtext-sm" />
+
+          <label htmlFor="password">Senha</label>
+          <InputText style={{ marginTop: '10px', border: '1px solid #ff9900', width: '300px' }} type="text" className="p-inputtext-sm" />
+          <Button style={{ marginTop: '15px', marginLeft: '215px', background: '#ff9900', borderColor: '#ff9900', color: 'black' }} label="Login" />
+        </div>
+      </div>
+    </div>
         )
     }
 }
